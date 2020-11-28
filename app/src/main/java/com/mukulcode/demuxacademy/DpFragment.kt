d3a503c30b571bcbf9528d0b197b44e3b8a6b34b
Mukul Kumar <mukul.2121@gmail.com>
2020-11-28 15:35:18 +0530

    added navigation component and binded views using view binding

--- app/src/main/java/com/mukulcode/demuxacademy/DpFragment.kt
+++ app/src/main/java/com/mukulcode/demuxacademy/DpFragment.kt
@@ -6,28 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
+import com.mukulcode.demuxacademy.databinding.FragmentDpBinding
 import com.mukulcode.demuxacademy.databinding.FragmentHomeBinding
-
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [DpFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
-
+class DpFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater, R.layout.fragment_home, container, false)
-
-        binding.dynamicProgrammingCard.setOnClickListener { view: View ->
-            Navigation.findNavController(view)
-                .navigate(HomeFragmentDirections.actionHomeFragment2ToDpFragment2())
-        }
+        val binding = DataBindingUtil.inflate<FragmentDpBinding>(inflater, R.layout.fragment_dp, container, false)
         return binding.root
     }
 }
--- app/src/main/java/com/mukulcode/demuxacademy/MainActivity.kt
+++ app/src/main/java/com/mukulcode/demuxacademy/MainActivity.kt
@@ -10,16 +10,22 @@ import org.eazegraph.lib.charts.PieChart
 import org.eazegraph.lib.models.PieModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val mPieChart = findViewById<View>(R.id.piechart) as PieChart
 
-        mPieChart.addPieSlice(PieModel("Todo", 65F, Color.parseColor("#007afe")))
-        mPieChart.addPieSlice(PieModel("Solved", 25F, Color.parseColor("#08a045")))
-        mPieChart.addPieSlice(PieModel("Attempted", 15F, Color.parseColor("#FBC233")))
-
-        mPieChart.startAnimation()
+//        val mPieChart = findViewById<View>(R.id.piechart) as PieChart
+//
+//        mPieChart.addPieSlice(PieModel("Todo", 65F, Color.parseColor("#007afe")))
+//        mPieChart.addPieSlice(PieModel("Solved", 25F, Color.parseColor("#08a045")))
+//        mPieChart.addPieSlice(PieModel("Attempted", 15F, Color.parseColor("#FBC233")))
+//
+//        mPieChart.startAnimation()
     }
 }
